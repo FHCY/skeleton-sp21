b054b63560d28cb8fd537b4acd4b02aff060d21b
FHCY <96365486+FHCY@users.noreply.github.com>
2024-11-02 21:03:39 +0800

    11.2

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -12,56 +12,28 @@ public class TestArrayDequeEC {
         ArrayDequeSolution<Integer> L1 = new ArrayDequeSolution<>();
         StudentArrayDeque<Integer> L2 = new StudentArrayDeque<>();
         int N = 500;
+        int randVal = StdRandom.uniform(0, 100);
+        String msg = "";
         for (int i = 0; i <= N; i++) {
             int random = StdRandom.uniform(0, 3);
             if (random == 0) {
                 // addFirst
-                int randVal = StdRandom.uniform(0, 100);
                 L1.addFirst(randVal);
                 L2.addFirst(randVal);
                 System.out.println("addFirst(" + randVal + ")");
             } else if (random == 1) {
                 // addLast
-                int randVal = StdRandom.uniform(0, 100);
                 L1.addLast(randVal);
                 L2.addLast(randVal);
                 System.out.println("addLast(" + randVal + ")");
-            }else if (L2.isEmpty() == false && random == 2) {
-                int actual = L2.removeFirst();
-                int expected = L1.removeFirst();
-                assertEquals("Oh noooo!\nThis is bad:\n   Random number " + actual
-                        + " not equal to " + expected + "!", expected, actual);
-            }
-        }
-    }
-
-    @Test
-    public void test_removeLast() {
-        ArrayDequeSolution<Integer> L1 = new ArrayDequeSolution<>();
-        StudentArrayDeque<Integer> L2 = new StudentArrayDeque<>();
-        int N = 500;
-        for (int i = 0; i <= N; i++) {
-            int random = StdRandom.uniform(0, 3);
-            if (random == 0) {
-                // addFirst
-                int randVal = StdRandom.uniform(0, 100);
-                L1.addFirst(randVal);
-                L2.addFirst(randVal);
-                System.out.println("addFirst(" + randVal + ")");
-            } else if (random == 1) {
-                // addLast
-                int randVal = StdRandom.uniform(0, 100);
-                L1.addLast(randVal);
-                L2.addLast(randVal);
-                System.out.println("addLast(" + randVal + ")");
-            } else if (L2.isEmpty()) {
-                continue;
-            } else if (L2.isEmpty() == false && random == 2) {
-                //removeLast
-                int actual = L2.removeLast();
-                int expected = L1.removeLast();
-                assertEquals("Oh noooo!\nThis is bad:\n   Random number " + actual
-                        + " not equal to " + expected + "!", expected, actual);
+            }else if (L2.size() != 0 && random == 2) {
+                if(randVal <50){
+                    msg +="removeFirst()\n";
+                    assertEquals(msg, L1.removeFirst(), L2.removeFirst());
+                } else {
+                    msg +="removeLast()\n";
+                    assertEquals(msg, L1.removeLast(), L2.removeLast());
+                }
             }
         }
     }
