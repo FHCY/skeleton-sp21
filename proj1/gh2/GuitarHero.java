5ff0f99884cb695ed2c00e9d3569b0921998810a
FHCY <96365486+FHCY@users.noreply.github.com>
2024-11-05 13:14:44 +0800

    11.5

--- proj1/gh2/GuitarHero.java
+++ proj1/gh2/GuitarHero.java
@@ -11,30 +11,29 @@ public class GuitarHero {
     private static Harp[] harps;
     private static Drum[] drums;
 
-    private static final int SIZE = 37;
-
-    public static double concert(int index) {
-        return 440 * Math.pow(2, (index - 24) / 12.0);
-    }
+    private static int SIZE = 37;
 
     private static void string() {
         strings = new GuitarString[SIZE];
         for (int i = 0; i < SIZE; i++) {
-            strings[i] = new GuitarString(concert(i));
+            double value = 440 * Math.pow(2, (i - 24) / 12.0);
+            strings[i] = new GuitarString(value);
         }
     }
 
     private static void harp() {
         harps = new Harp[SIZE];
         for (int i = 0; i < SIZE; i++) {
-            harps[i] = new Harp(concert(i));
+            double value = 440 * Math.pow(2, (i - 24) / 12.0);
+            harps[i] = new Harp(value);
         }
     }
 
     private static void drum() {
         drums = new Drum[SIZE];
         for (int i = 0; i < SIZE; i++) {
-            drums[i] = new Drum(concert(i));
+            double value = 440 * Math.pow(2, (i - 24) / 12.0);
+            drums[i] = new Drum(value);
         }
     }
 
@@ -42,37 +41,35 @@ public class GuitarHero {
         string();
         harp();
         drum();
-        while (true) {
-            
-            play(strings);
-            //play(harps);
-            //play(drums);
-        }
+        play(strings);
+        //play(harps);
+        //play(drums);
+
     }
 
     private static void play(GuitarString[] t) {
-        if (StdDraw.hasNextKeyTyped()) {
-            char key = StdDraw.nextKeyTyped();
-            int index = KEYBOARD.indexOf(key);
-            if (index >= 0) {
-                t[index].pluck();
+        while (true) {
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int index = KEYBOARD.indexOf(key);
+                if (index >= 0) {
+                    t[index].pluck();
+                }
+            }
+
+            /* compute the superposition of samples */
+            double sample = 0;
+            for (GuitarString elem : t) {
+                sample += elem.sample();
+            }
+
+            /* play the sample on standard audio */
+            StdAudio.play(sample);
+
+            /* advance the simulation of each guitar string by one step */
+            for (GuitarString elem : t) {
+                elem.tic();
             }
         }
-
-        /* compute the superposition of samples */
-        double sample = 0;
-        for (GuitarString elem : t) {
-            sample += elem.sample();
-        }
-
-        /* play the sample on standard audio */
-        StdAudio.play(sample);
-
-        /* advance the simulation of each guitar string by one step */
-        for (GuitarString elem : t) {
-            elem.tic();
-        }
     }
-
-
 }
